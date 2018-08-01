fd2357a5952f6f5eeb729e4a708787fee69e2567
Erik Kvale <eirikval@gmail.com>
2018-08-01 12:20:35 -0500

    Updated route for index to render template with specified context vars

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Erik."
+    user = {'username': 'Erik'}
+    return render_template("index.html", title='Home', user=user)
