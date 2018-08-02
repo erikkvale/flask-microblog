f918a3e39c74dd0a7dcf0a859d1409ce0c1bb61e
Erik Kvale <eirikval@gmail.com>
2018-08-02 11:24:03 -0500

    Changed database environ name for local postgres, i.e. numerous flavors on my local host, generic I prefer not.

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
+    SQLALCHEMY_DATABASE_URI = (os.environ.get('POSTGRES_DB_URL') or
         'sqlite:///' + os.path.join(BASE_DIR, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
