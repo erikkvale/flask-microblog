e6c302213b74df4f75c92542dec57d60e930d99e
Erik Kvale <eirikval@gmail.com>
2018-08-02 15:06:22 -0500

    Added mixin to User class

--- app/models.py
+++ app/models.py
@@ -3,10 +3,11 @@ from werkzeug.security import (
     generate_password_hash,
     check_password_hash
 )
+from flask_login import UserMixin
 from app import db
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
