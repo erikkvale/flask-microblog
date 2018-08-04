b43ed6ad4a014d65a3774198f9ebb7cca9e0ab0f
Erik Kvale <eirikval@gmail.com>
2018-08-04 10:40:28 -0500

    Removed user var and context ref

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,6 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Erik'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -32,7 +31,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
