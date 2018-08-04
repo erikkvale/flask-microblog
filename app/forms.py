bf0aaf03fe9bfe1cb17968c4973156b1867b8001
Erik Kvale <eirikval@gmail.com>
2018-08-04 10:45:06 -0500

    Changed import statement formatting

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import (
-    StringField, PasswordField,
-    BooleanField, SubmitField
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField
 )
 from wtforms.validators import DataRequired
 
