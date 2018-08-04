6c98cb0668288e2c077ba278cbfdef84d55e1c9a
Erik Kvale <eirikval@gmail.com>
2018-08-04 10:35:59 -0500

    Adjusted import format statements and added new section in login view to properly redirect users after login, including to the url they were on when they were forced to login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user, login_required
+from flask import (
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    request
+)
+from flask_login import (
+    current_user,
+    login_user,
+    logout_user,
+    login_required
+)
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
 from app.models import User
@@ -35,6 +47,9 @@ def login():
             return redirect(url_for('login'))
         else:
             login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
             return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
