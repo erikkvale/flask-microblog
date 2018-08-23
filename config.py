2ff821ba31f5a2223468e36e2eabbe16dfc36b61
Erik Kvale <eirikval@gmail.com>
2018-08-23 14:22:44 -0500

    Added email settings

--- config.py
+++ config.py
@@ -4,6 +4,16 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Db settings
     SQLALCHEMY_DATABASE_URI = (os.environ.get('POSTGRES_DB_URL') or
         'sqlite:///' + os.path.join(BASE_DIR, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email settings
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['eirikval@gmail.com']
